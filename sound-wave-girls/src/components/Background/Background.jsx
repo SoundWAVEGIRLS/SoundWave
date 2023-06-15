56175a147fc9d27a33f658237fb472f774195e85
mgblanco10 <mgblanco1001@gmail.com>
2023-06-15 15:13:25 +0200

    backgroundHome finish ball and initial of useLocation changePage

---
+++ sound-wave-girls/src/components/Background/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Background.css'
+// import { useLocation } from 'react-router-dom';
+
+
+function Background() {
+    // const location = useLocation();
+    // const backgroundImage = location.pathname === '/other' ? 'background2.svg' : 'background1.svg';
+
+  return (
+    <div className="backgroundHome" ></div>
+  );
+}
+
+export default Background;
