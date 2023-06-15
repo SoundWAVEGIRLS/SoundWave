9fe3c57646d5cdf5d536c56a7fcf07477c8b5094
Nelora Mood <lorenamoralesodena@gmail.com>
2023-06-15 12:09:28 +0200

    WIP button functionComponent butomJoinNow

---
+++ sound-wave-girls/src/components/butom-joinNow/butomJoinNow.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+
+
+// function butomJoinNow() {
+//   return (
+//     <div className=''>butomJoinNow</div>
+//     <button type='button' onClick={butomJoinNow}
+//   )
+// }
+
+// export default butomJoinNow
