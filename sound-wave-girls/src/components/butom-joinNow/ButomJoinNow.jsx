84e9d05e706cec206183e3b49bf0cb4edf06461f
Nelora Mood <lorenamoralesodena@gmail.com>
2023-06-19 10:09:51 +0200

    rendered button with commented css

---
+++ sound-wave-girls/src/components/butom-joinNow/ButomJoinNow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ButomJoinNow.css';
+
+function ButomJoinNow() {
+    // const handleButtonClick = () => {
+
+    // }
+  
+    return (
+      <button type="button" className='btnJoinNow' >
+        Join Now
+      </button>
+    );
+  }
+
+export default ButomJoinNow
