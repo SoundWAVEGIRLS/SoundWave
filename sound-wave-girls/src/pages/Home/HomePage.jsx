36c2da6f928a29109aee4d890de41d3ae0580052
valblue21 <132341389+valblue21@users.noreply.github.com>
2023-06-21 12:57:24 +0200

    modificar rutas de import, pagina HomePage.jsx incliyendo buttonJoinNow

--- sound-wave-girls/src/pages/Home/HomePage.jsx
+++ sound-wave-girls/src/pages/Home/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import BackgroundHome from '../components/BackgroundHome/BackgroundHome'
-import GirlMain from '../components/GirlMain/GirlMain'
-import TitleInfoHome from '../components/TitleInfoHome/TitleInfoHome'
+import BackgroundHome from '../../components/BackgroundHome/BackgroundHome'
+import GirlMain from '../../components/GirlMain/GirlMain'
+import TitleInfoHome from '../../components/TitleInfoHome/TitleInfoHome'
 import './HomePage.css'
-
-import NavBar from '../components/NavBar/NavBar'
-import ButomJoinNow from '../components/ButomJoinNow/ButomJoinNow'
+import NavBar from '../../components/NavBar/NavBar'
+import ButomJoinNow from '../../components/ButomJoinNow/ButomJoinNow'
 
 function HomePage() {
   return (
@@ -14,11 +13,10 @@ function HomePage() {
           <GirlMain />
           </BackgroundHome>
           <NavBar />
-          <TitleInfoHome />
           <ButomJoinNow />
+          <TitleInfoHome />
     </div>
   )
 }
 
 export default HomePage
-{/* <Link to="/discover" className='ruta'> Ir a Discover </Link> */}
