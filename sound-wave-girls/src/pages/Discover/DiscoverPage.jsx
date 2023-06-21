c86ea774f38888aa05b274b823af1a9badaff10d
valblue21 <132341389+valblue21@users.noreply.github.com>
2023-06-21 12:45:55 +0200

    cambio de rutas en import de DiscoverPage.jsx

--- sound-wave-girls/src/pages/Discover/DiscoverPage.jsx
+++ sound-wave-girls/src/pages/Discover/DiscoverPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import './DiscoverPage.css'
-import Footer from '../components/Footer/Footer'
-
-import DiscoverImage from '../components/DiscoverImage/DiscoverImage'
-import NavBar from '../components/NavBar/NavBar'
-import ButtonMainDiscover from '../components/ButtonMainDiscover/ButtonMainDiscover'
-
+import ButtonMainDiscover from '../../components/ButtonMainDiscover/ButtonMainDiscover'
+import DiscoverImage from '../../components/DiscoverImage/DiscoverImage'
+import NavBar from '../../components/NavBar/NavBar'
+import Footer from '../../components/Footer/Footer'
 function DiscoverPage() {
   return (
     <div className='discoverPage'> 
