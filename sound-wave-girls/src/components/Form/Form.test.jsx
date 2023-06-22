50e676959c1d093ea93fa78a142a69602a0057f8
mgblanco10 <mgblanco1001@gmail.com>
2023-06-22 10:00:00 +0200

    finish test of form

--- sound-wave-girls/src/components/Form/Form.test.jsx
+++ sound-wave-girls/src/components/Form/Form.test.jsx
@@ -1,36 +1,28 @@
 import { beforeEach, expect } from "vitest"
 import {render, screen,fireEvent } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { MemoryRouter } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import Form from "./Form"
 
 describe('Component Form', () => {
+    beforeEach(() => {
+      render(<Form />);
+    });
 
     test('Form submission', () => {
-        beforeEach(() => {
-          render(<Form />);
-        });
-      
         expect(screen.getByRole('button', { name: 'Join Now' })).toBeDefined();
       });
   
       test('submit_todos_los_campos', () => {
-        render(<Contact />);
-    
+        
         const nameInput = screen.getByLabelText("Name:");
         const emailInput = screen.getByLabelText("Email:");
-        const messageInput = screen.getByLabelText("Message:");
-        const sendButton = screen.getByRole("button", {name: "Send"});
-    
-        fireEvent.change(nameInput, {target: {value: "Lorena Romero"}});
-        fireEvent.change(emailInput, {target: {value: "l.r.@hola.com"}});
-        fireEvent.change(messageInput, {target: {value: "Hola, estoy interesada en comprar los auriculares."}});
-        fireEvent.click(sendButton);
-    
-        expect(screen.getByText("Gracias por tu mensaje")).toBeInTheDocument();
-    
-    })
+        const passwordInput = screen.getByLabelText("Password:");
+      
+        fireEvent.change(nameInput, { target: { value: "Lorena Romero" } });
+        fireEvent.change(emailInput, { target: { value: "l.r.@hola.com" } });
+        fireEvent.change(passwordInput, { target: { value: "123123123" } });
+
+        expect(screen.getByText('Se ha registrado correctamente')).toBeDefined()
+      });
 
 
   });
